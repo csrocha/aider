c510aaf537518cf5322bf7a572385c2e532189c8
Cristian Sebastian Rocha <csrocha@gmail.com>
2026-02-16 03:32:40 -0300

    Fix move readonly to editable on submodules file.

--- aider/commands.py
+++ aider/commands.py
@@ -868,7 +868,7 @@ class Commands:
                 self.io.tool_error(f"{matched_file} is already in the chat as an editable file")
                 continue
             elif abs_file_path in self.coder.abs_read_only_fnames:
-                if self.coder.repo and self.coder.repo.path_in_repo(matched_file):
+                if self.coder.repo and self.coder.repo.path_in_repo(matched_file, submodules=True):
                     self.coder.abs_read_only_fnames.remove(abs_file_path)
                     self.coder.abs_fnames.add(abs_file_path)
                     self.io.tool_output(
